bbae8f9d2b8fd539da651be6db2b5455615bc956
NicoA2002 <114366568+NicoA2002@users.noreply.github.com>
2024-11-19 22:20:49 -0500

    Organized file, signal reaches microphones
    Divided up file into sections that need to be developed, cleaned up, and applied necessary delay and attenuation to reach microphones

--- BeamformingSims/MicArraySim.m
+++ BeamformingSims/MicArraySim.m
@@ -1,11 +1,19 @@
 clear;
-
 verbose = true;
 speak = false;
-% Step 1: Audio Source 
 
-target_pos = [0, 1];
+%   NUMBER AND POSITIONS OF MICROPHONES IN METERS  %
+mic1 = [0 0];
+mic2 = [-72.6e-3 -10e-3];
+mic3 = [72.6e-3 -10e-3];
+mic_pos = [mic1; mic2; mic3];
+
+%   POSITION OF AUDIO SOURCE    %
+target_pos = [0, 2];
+
 [target_audio, Fs] = audioread('recorded_audio.wav');
+n = length(target_audio);
+t = (0:n-1)/Fs;
 
 if speak
     sound(target_audio, Fs);
@@ -14,26 +22,75 @@ end
 % Step 2: Delay and attenuation due to distance from audio source
 % attenuation by 1/(4pi*r)
 %positions x,y in meters, rough estimates from my glasses
-% center mic, left mic, right mic
-mic1 = [0 0];
-mic2 = [-72.6e-3 -10e-3];
-mic3 = [72.6e-3 -10e-3];
-mic_pos = [mic1; mic2; mic3];
 
-%distance of each
+%distance of each from target audio
 mic_d = vecnorm((mic_pos - target_pos)');
 
-%delay of each
+% Subplot 1: Time-domain representation
+figure;
+
+subplot(4, 1, 1);
+plot(t, target_audio);
+title('Time-Domain Representation of Audio Signal');
+xlabel('Time (s)');
+ylabel('Amplitude');
+grid on;
+
+%delay of each and graph
 SOUND = 343; %meters/second
 mic_delay = mic_d/SOUND; 
+mic_data = zeros(n, length(mic_delay));
+for i = 1:length(mic_delay)
+    delay = int32(mic_delay(i)*Fs);
 
-%sample delay: seconds * Fs
-mic_data = mic_delay * Fs;
+    %insert delay # of samples before and attenuate by 4pi*distance
+    mic_data(:,i) = [zeros(delay,1); target_audio(1:end-delay)]/(4*pi*mic_d(i));
+
+    subplot(4, 1, i+1);
+    plot(t, mic_data(:,i));
+    title(sprintf('Mic_{%d}, Delay %f ms', i, round(mic_delay(i)*1e3,3)));
+    xlabel('Time (s)');
+    ylabel('Amplitude');
+    grid on;
+end
+
 if verbose
     fprintf("The microphones have delays of %s microseconds relative to mic1\n",join(string(abs(diff(mic_delay(1:2)))*1e6), ', ')); 
 end
 
-% Model Microphone Transfer Function, Fs=768kHz 
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%                       SIGNAL REACHES MICROPHONES                       %
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+% MICROPHONE CONTINUOUS TIME H(s)
+
+
+% ANTIALIASING FILTER
+
+
+% SAMPLING
+
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%                         SIGNAL CONVERTED TO DIGITAL                    %
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+% BEAMFORMING OPERATIONS
+
+% delay and sum --> simplest, least computationally intensive
+
+% Bartlett Beamforming --> delay/sum + weighting per microphone
+
+% frequency domain delay/sum + deconvolution --> more computation, cleaner
+
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%                         COMPARISONS AND ANALYSIS                       %
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+% COMPARISON BETWEEN EACH
+
+
 % fft audio data, multiply amplitude add phase
 
 % Frequency points (Hz)
@@ -48,42 +105,6 @@ phase_deg = [62 37 22 18 16 8 5 3 1 0.5 0 -1 -2 -4.8 -5.1 -5.2];
 evalFunction(magnitudeCoeffs, 20)
 
 
-% Plot Audio in Time and Frequency Domain
-% Step 1: Load the audio file
-
-target_audio = target_audio(:, 1);  % Use only the first channel if stereo
-
-% Step 2: Define time vector
-n = length(target_audio);     % Number of samples
-t = (0:n-1) / Fs;       % Time vector
-
-% Step 3: Compute the FFT of the signal
-y_fft = fft(target_audio);    % Perform FFT
-P2 = abs(y_fft/n);      % Two-sided spectrum
-P1 = P2(1:n/2+1);       % One-sided spectrum
-P1(2:end-1) = 2*P1(2:end-1); % Adjust amplitude for one-sided spectrum
-
-% Step 4: Define the frequency axis
-f = Fs * (0:(n/2)) / n; % Frequency vector
-
-% Step 5: Plot the time-domain signal and its frequency spectrum
-figure;
-
-% Subplot 1: Time-domain representation
-subplot(2, 1, 1);
-plot(t, target_audio);
-title('Time-Domain Representation of Audio Signal');
-xlabel('Time (s)');
-ylabel('Amplitude');
-grid on;
-
-% Subplot 2: Frequency-domain representation
-subplot(2, 1, 2);
-plot(f, P1);
-title('Frequency-Domain Representation (FFT) of Audio Signal');
-xlabel('Frequency (Hz)');
-ylabel('|P1(f)|');
-grid on;
 
 
 
