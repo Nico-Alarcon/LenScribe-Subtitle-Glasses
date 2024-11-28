5c305c74f244a9bf2b791290678ab697cef76cb3
NicoA2002 <114366568+NicoA2002@users.noreply.github.com>
2024-11-28 01:01:46 -0500

    working up until beamforming input, verify sampling
    Sampling looks like it has the correct number of samples in the size of the new matrix so im sure tis fine but check with a graph

--- BeamformingSims/MicArraySim.m
+++ BeamformingSims/MicArraySim.m
@@ -7,6 +7,7 @@ mic1 = [0 0];
 mic2 = [-72.6e-3 -10e-3];
 mic3 = [72.6e-3 -10e-3];
 mic_pos = [mic1; mic2; mic3];
+mic_n = 3;
 
 %   POSITION OF AUDIO SOURCE    %
 target_pos = [0, 2];
@@ -14,6 +15,7 @@ target_pos = [0, 2];
 [target_audio, Fs] = audioread('recorded_audio.wav');
 n = length(target_audio);
 t = (0:n-1)/Fs;
+f = (0:n-1)*(Fs/n); % Frequency axis (Hz)
 
 if speak
     sound(target_audio, Fs);
@@ -39,8 +41,8 @@ grid on;
 %delay of each and graph
 SOUND = 343; %meters/second
 mic_delay = mic_d/SOUND; 
-mic_data = zeros(n, length(mic_delay));
-for i = 1:length(mic_delay)
+mic_data = zeros(n, mic_n);
+for i = 1:mic_n
     delay = int32(mic_delay(i)*Fs);
 
     %insert delay # of samples before and attenuate by 4pi*distance
@@ -51,7 +53,7 @@ for i = 1:length(mic_delay)
     title(sprintf('Mic_{%d}, Delay %f ms', i, round(mic_delay(i)*1e3,3)));
     xlabel('Time (s)');
     ylabel('Amplitude');
-    grid on;
+    grid on; hold on;
 end
 
 if verbose
@@ -62,35 +64,60 @@ end
 %                       SIGNAL REACHES MICROPHONES                       %
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
-beam_input = mic_data;
-% MICROPHONE CONTINUOUS TIME H(s) TODO
 
-f = [35, 100, 900, 1000, 1100, 8000, 15000];
-% Amplitude response in dB (relative to a reference, e.g., 1kHz)
+% MICROPHONE CONTINUOUS TIME H(s)  
+
+f_approx = [35, 100, 900, 1000, 1100, 8000, 15000]; %Hz
 magnitude_dB = [-1.0, -0.5, 0.0, 0.0, 0.0, 1.5, 4.5];
-% Phase response (degrees)
 phase_deg = [0, 0, 0, 0, 0, 0, 0,];
-
-% Call the function
-[magnitudeCoeffs, phaseCoeffs] = fitFrequencyResponse(f, magnitude_dB, phase_deg, 5);
+% Mic Frequency Response
+[magnitudeCoeffs, phaseCoeffs] = fitFrequencyResponse(f_approx, magnitude_dB, phase_deg, 5);
+mic_H = (10.^(evalFunction(magnitudeCoeffs,f)/20).*exp(1j*pi*evalFunction(phaseCoeffs,f)/180))';
 
 % ANTIALIASING FILTER, can be changed once solidified
 N=6;
-fp = 8000; %cutoff frequency
+fp = 2*pi*8000; %cutoff frequency
 [z_butter, p_butter, k_butter] = buttap(N);
-[num_butter, den_butter] = zp2tf(z_butter*fp, p_butter*fp, k_butter);
-freqs(num_butter, den_butter);
+[num_butter, den_butter] = zp2tf(z_butter*fp, p_butter*fp, k_butter*(fp^N));
 AAF = tf(num_butter, den_butter);
+[H, ~] = freqresp(AAF,2*pi*f);
+H = squeeze(H);
 
-beam_input = lsim(AAF, beam_input, t); 
+%%%%%%%%%%% Apply Microphone and AAF tfs %%%%%%%%%%%%%%%%%%
+input_H = H .* mic_H;
+beam_input = fft(mic_data) .* repmat(input_H, 1,mic_n);
+
+%%%%%%%%%%% FFT Graphs  %%%%%%%%%%%%%%%% Should Look V Similar
+audio_fft = abs(fft(target_audio)); % Compute the FFT
+beam_fft = abs(beam_input(:,1));
+
+figure;
+subplot(2,1,1);
+semilogx(f(1:n/2), 20*log10(audio_fft(1:n/2,1))); % Plot only the positive frequencies
+xlabel('Frequency (Hz)');
+ylabel('Magnitude (dB)');
+ylim([-200, 100]);
+title('Magnitude Spectrum of Audio Signal');
+subplot(2,1,2);
+
+semilogx(f(1:n/2), 20*log10(beam_fft(1:n/2)), 'DisplayName','Beamforming Input'); hold on;
+semilogx(f(1:n/2), 20*log10(abs(H(1:n/2))), 'b', 'LineWidth', 2,'DisplayName', 'AAF Filter');
+semilogx(f(1:n/2), 20*log10(abs(mic_H(1:n/2))), 'r' , 'LineWidth', 2, 'DisplayName', 'Microphone H(s)');
+xlabel('Frequency (Hz)');
+ylabel('Magnitude (dB)');
+legend('Location', 'best');
+ylim([-200, 100]);
+title('Magnitude Spectrum of Audio Signal After Mics and AAF');
+
+%%%%%%%%%%%%%%% FFT Check %%%%%%%%%%%%%
 
 % SAMPLING - uses its own AAF, but doesnt matter if fs > 16kHz
 fs_adc = 20000;
 ts = (0:n-1)/fs_adc;
 [p, q] = rat(fs_adc / Fs); % Calculate resampling factors
 
-beam_input = resample(beam_input', p, q)'; % Resample signal
-
+beam_input = ifft(beam_input);
+beam_input = resample(beam_input, p, q); % Resample signal
 
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 %                         SIGNAL CONVERTED TO DIGITAL                    %
@@ -123,6 +150,14 @@ beam_input = resample(beam_input', p, q)'; % Resample signal
 
 %%%%%%%                 HELPER FUNCTIONS                   %%%%%%%%%%%
 function out = evalFunction(coeff, f)
+    %from an array of coefficients evaluate function with log input
+    out = 0;
+    for i = 1:length(coeff)
+        out = out + coeff(i)*log10(f).^(length(coeff)-i);
+    end
+end
+
+function out = evalFunct(coeff, f)
     %from an array of coefficients evaluate function with log input
     out = 0;
     for i = 1:length(coeff)
